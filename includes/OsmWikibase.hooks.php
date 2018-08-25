4b39ca2a261e2c068c588742755b6412b8fe6fc1
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2018-08-26 01:52:12 +0300

    added wikibase styling

--- includes/OsmWikibase.hooks.php
+++ includes/OsmWikibase.hooks.php
@@ -31,4 +31,27 @@ class Hooks
 
 		return true;
 	}
+
+	/**
+	 * Adds CSS to customize Wikibase display
+	 * @param \OutputPage &$out
+	 * @param \Skin &$skin
+	 * @return bool
+	 */
+	public static function onBeforePageDisplay(
+		/** @noinspection PhpUnusedParameterInspection */ &$out, &$skin
+	) {
+		$out->addModuleStyles( 'ext.OsmWikibase-all' );
+
+		$title = $out->getTitle();
+		if ( $title ) {
+			$ns = $title->getNamespace();
+			if ( $ns === WB_NS_ITEM || $ns === WB_NS_PROPERTY ) {
+				$out->addModuleStyles( 'ext.OsmWikibase' );
+			}
+		}
+
+		return true;
+	}
+
 }
