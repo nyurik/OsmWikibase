a1988dc818e66989edb378675d9af59ee1fb8050
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2018-08-26 05:51:02 +0300

    moved custom prefix code into a branch

--- README.md
+++ README.md
@@ -2,6 +2,10 @@ MediaWiki extension to customize the Wikibase on OSM Wiki.
 
 See [OSM Wikibase project page](https://wiki.openstreetmap.org/wiki/OpenStreetMap:Wikibase)
 
-Implemented:
--  Overrides default ItemId to use `Y` instead of `Q` prefix
+### Implemented
 -  Style sitelink section to only show "other" section without the header and the edit links at at the top right, adjusted for multiple skins
+
+### Work in progress
+-  Overrides default ItemId to use `Y` instead of `Q` prefix
+
+This turned out to be much more difficult than anticipated. The work has been moved to the `customPrefix` branch.  The proposed solution works for creating and editing items, but it breaks when a sitelink needs to be resolved into a Wikibase ID. 
--- includes/OsmWikibase.hooks.php
+++ includes/OsmWikibase.hooks.php
@@ -12,27 +12,6 @@ namespace OsmWikibase;
 class Hooks
 {
 	/**
-	 * WikibaseRepoEntityTypes hook handler.
-	 *
-	 * @param array $entityTypeDefinitionsArray
-	 * @return bool
-	 */
-	public static function onWikibaseRepoEntityTypes( &$entityTypeDefinitionsArray ) {
-
-		$entityTypeDefinitionsArray['item']['entity-id-pattern'] = OsmItemId::PATTERN;
-
-		$entityTypeDefinitionsArray['item']['entity-id-builder'] = function ( $serialization ) {
-			return new OsmItemId( $serialization );
-		};
-
-		$entityTypeDefinitionsArray['item']['entity-id-composer-callback'] = function ( $repositoryName, $uniquePart ) {
-			return OsmItemId::newFromRepositoryAndNumber( $repositoryName, $uniquePart );
-		};
-
-		return true;
-	}
-
-	/**
 	 * Adds CSS to customize Wikibase display
 	 * @param \OutputPage &$out
 	 * @param \Skin &$skin
